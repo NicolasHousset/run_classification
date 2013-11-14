55c689086008f1122234e1d0fe99be61d5425ce6
NicolasHousset <nicolas.housset@ugent.be>
2013-11-14 16:44:24 +0100

    update

--- source/ELUDE_Shift.R
+++ source/ELUDE_Shift.R
@@ -56,14 +56,19 @@ setkey(rtPeptide, sequence, modified_sequence)
 rtPeptide <- rtPepUnique[rtPeptide]
 
 # rtPeptide <- rtPeptide[mod1=="" | mod1=="K<prop>" | mod1=="K<propC13>"]
-rtPeptide <- rtPeptide[mod1=="" & modN=="NH2-" & modC=="-COOH"]
-rtPeptide <- rtPeptide[(mod1=="M<Mox*>" | mod1=="" | mod1=="C<Cmm*>" | mod1=="K<c13>" | mod1=="R<C13>" | mod1=="Q<Pyr>") &
-                         modN=="NH2-" & mod2!="M<Mox>" & mod3!="M<Mox>" & mod4!="M<Mox>"]
+# rtPeptide <- rtPeptide[mod1=="" & modN=="NH2-" & modC=="-COOH"]
+rtPeptide <- rtPeptide[(mod1=="M<Mox*>" | mod1=="M<Mox>" | mod1=="" | mod1=="C<Cmm*>" | mod1=="K<c13>" | mod1=="R<C13>" | mod1=="Q<Pyr>") &
+                         modN=="NH2-"]
 
 rtPeptide[substr(modified_sequence, 1, 4) == "Ace-", elude_sequence := paste0(substr(modified_sequence, 5,5),"[Ace]",substr(modified_sequence, 6, nchar(modified_sequence)))]
 rtPeptide[substr(modified_sequence, 1, 4) == "NH2-", elude_sequence := paste0(substr(modified_sequence, 5,5),"",substr(modified_sequence, 6, nchar(modified_sequence)))]
 rtPeptide[substr(modified_sequence, 1, 5) == "prop-", elude_sequence := paste0(substr(modified_sequence, 6,6),"[prop]",substr(modified_sequence, 7, nchar(modified_sequence)))]
 rtPeptide[substr(modified_sequence, 1, 7) == "propC13-", elude_sequence := paste0(substr(modified_sequence, 8,8),"[prop]",substr(modified_sequence, 9, nchar(modified_sequence)))]
+
+rtPeptide[, elude_sequence := gsub("Mox\\*","15,994915", elude_sequence)]
+rtPeptide[, elude_sequence := gsub("Mox","15,994915", elude_sequence)]
+rtPeptide[, elude_sequence := gsub("Cmm\\*","57,021464", elude_sequence)]
+rtPeptide[, elude_sequence := gsub("Pyr","-17", elude_sequence)]
 
 rtPeptide[, elude_sequence := gsub("<","[",elude_sequence)]
 rtPeptide[, elude_sequence := gsub(">","]",elude_sequence)]
@@ -74,11 +79,9 @@ rtPeptide <- rtPeptide[!is.na(elude_sequence)]
 rtPeptide[, error := grepl("\\]\\[", elude_sequence)]
 rtPeptide <- rtPeptide[error == FALSE]
 
-# To exclude redundant peptides
-rtPeptide <- rtPeptide[index_rt2 <2]
 # The filter on rt makes median too compact at the end
 # Also, better to filter out the very beginning
-rtPeptide <- rtPeptide[q50_5 > 750 & q50_5 < 1750]
+# rtPeptide <- rtPeptide[q50_5 > 750 & q50_5 < 1750]
 
 setkey(rtPeptide, elude_sequence)
 rtPeptideUnique <- unique(rtPeptide)[, list(elude_sequence,q50_3,q50_5)]
@@ -91,7 +94,7 @@ middle <- as.character(11:17)
 earlyCoefficient <- 1
 lateCoefficient <- 1
 middleCoefficient <- 1
-threshold <- 0.03
+threshold <- 0.015
 
 breaks <- (7:25)*100
 rtPeptideUnique[, rtCent := cut(x=q50_5, breaks=((7:25)*100), labels=7:24)]
@@ -108,10 +111,17 @@ rtPeptideUnique[late, train := (trainRand < (threshold * lateCoefficient))]
 # ggplot(rtPeptideUnique[train==TRUE], aes(rtCent)) + geom_histogram()
 
 rtPeptideTrain <- rtPeptideUnique[train==TRUE, list(elude_sequence, q50_5)]
+minTrain = min(rtPeptideTrain[, q50_5])
+maxTrain = max(rtPeptideTrain[, q50_5])
+subFactor = mean(c(minTrain, maxTrain))
+divFactor = maxTrain - subFactor
+rtPeptideTrain[, q50_5 := (q50_5 - subFactor) / divFactor]
+
 # modified_sequence is included because it's the key, but I suspect this kind of behavior to change, so check.
 # We want to assess the performance of the algorithm on the actual retention time observed, not the median.
 setkey(rtPeptide, elude_sequence)
 rtPeptideTest <- rtPeptide[rtPeptideUnique[train == FALSE, elude_sequence], corrected_RT]
+rtPeptideTest[, corrected_RT := (corrected_RT - subFactor) / divFactor]
 
 setkey(rtPeptideUnique, elude_sequence)
 setkey(rtPeptide, elude_sequence)
@@ -159,11 +169,13 @@ shell(strCommand, translate = TRUE, wait = TRUE)
 
 
 rtPeptideTestMedian  <- unique(rtPeptide[rtPeptideUnique[train == FALSE, elude_sequence]])[, list(elude_sequence,q50_5)]
+rtPeptideTestMedian[, q50_5 := (q50_5 - subFactor) / divFactor]
 write.table(rtPeptideTestMedian, file=paste0(projectPath, "/data/rtPeptideTestMedian.txt"), quote = FALSE, , sep="\t", row.names = FALSE, col.names = FALSE)
+# Let's assess the performance on 
 testData <- "/data/rtPeptideTestMedian.txt"
 testData <- shQuote(paste0(projectPath, testData))
 
-savePredict <- "/data/predictionsTestMedian.out"
+savePredict <- "/data/predictionsTest.out"
 savePredict <- shQuote(paste0(projectPath, savePredict))
 
 loadModelFlag <- " -l "
@@ -176,7 +188,7 @@ strCommand <- paste0("cd ",shQuote(eludePath), " && elude ", verbFlag, verbLevel
 shell(strCommand, translate = TRUE, wait = TRUE)
 
 
-results <- data.table(read.table(file=paste0(projectPath, "/data/predictionsTestMedian.out"), header = TRUE, sep = "\t"))
+results <- data.table(read.table(file=paste0(projectPath, "/data/predictionsTest.out"), header = TRUE, sep = "\t"))
 results[, diff := Predicted_RT - Observed_RT]
 setkey(results, Observed_RT)
 
@@ -184,13 +196,19 @@ setkey(results, Observed_RT)
 results[, centile := 1:NROW(results)]
 results[, centile := ceiling(100 * centile / NROW(results))]
 results[, meanError := mean(diff), by = centile]
-results[, q95_pred := quantile(diff, probs = 0.975) - quantile(diff, probs = 0.025), by = centile]
-results[, q90_pred := quantile(diff, probs = 0.95) - quantile(diff, probs = 0.05), by = centile]
+results[, q975 := quantile(diff, probs = 0.975), by = centile]
+results[, q025 := quantile(diff, probs = 0.025), by = centile]
+results[, q500 := quantile(diff, probs = 0.500), by = centile]
+results[, q250 := quantile(diff, probs = 0.250), by = centile]
+results[, q750 := quantile(diff, probs = 0.750), by = centile]
+
+
 ggplot(results, aes(Observed_RT, meanError)) + geom_point(alpha=(1/2))
-ggplot(results, aes(centile, q95_pred)) + geom_point(alpha=(1/2))
+ggplot(results, aes(centile, q500)) + geom_point(alpha=(1/2))
 ggplot(results, aes(centile, q90_pred)) + geom_point(alpha=(1/2))
 
 
+# Very long to compute, ok with testmedian, not ok with test
 for(i in 400:(NROW(results))){
   results[i, movq975 := results[(i-399):i, quantile(diff, probs = 0.975)]]
   results[i, movq025 := results[(i-399):i, quantile(diff, probs = 0.025)]]
@@ -201,13 +219,12 @@ for(i in 400:(NROW(results))){
 
 graphDS <- melt(data = results, 
                 id.vars = c("Peptide", "Observed_RT","Predicted_RT"), 
-                measure.vars = c("movq025","movq250","movq500","movq750","movq975"),
+                measure.vars = c("q025","q250","q500","q750","q975"),
                 variable.name = "quantile",
                 value.name = "error")
 
-ggplot(graphDS, aes(Observed_RT, error, colour = quantile)) + geom_point(alpha=(1/2)) + xlim(750,1750)+ ylim(-300,160)
-ggplot(results, aes(Observed_RT, movq975 - movq025)) + geom_point(alpha=(1/2)) + xlim(750,1750)
+ggplot(graphDS, aes(Observed_RT, error, colour = quantile)) + geom_point(alpha=(1/2)) + xlim(-1,1)+ ylim(-0.6,0.3)
 
-save(graphDS, file = paste0(projectPath,"/plot/graphLow.RData"), compression_level = 1)
+save(graphDS, file = paste0(projectPath,"/plot/graphCorrected.RData"), compression_level = 1)
 
 ggplot(rtPeptideUnique, aes(q50_5)) + xlim(700,1900) + geom_histogram(aes(y = ..density..), binwidth = 10)
--- source/classification_shift_implementation.R
+++ source/classification_shift_implementation.R
@@ -1,3 +1,4 @@
+projectPath <- "C:/Users/Nicolas Housset/Documents/R_Projects/run_classification"
 load(file = paste0(projectPath,"/data/corrected_id.RData"))
 
 # Finally, the awaited correction of the shift...
@@ -17,12 +18,22 @@ compute_correction <- function(raw_rt,quartile, ...){
   return(list_correction[quartile, correction] + ((raw_rt- list_correction[quartile, quartiles])/(list_correction[quartile+1, quartiles]-list_correction[quartile, quartiles])) * (list_correction[quartile+1,correction] - list_correction[quartile,correction]))
 }
 
+# Initializing the database of corrections applied
+dt_correction <- data.table(1:5)
+dt_correction[, quartiles := 1:5]
+dt_correction[, correction := 0.0]
+dt_correction[, group := "high07"]
+dt_correction[, V1 := NULL]
+
 # The group chosen as reference is obviously not included
 list_group <- list("low01","low02","low03","low04","low05","low06","low07","low08",
                    "high01","high02","high03","high04","high05","high06", "high08")
+# Getting common peptides for each reference/non-reference pair and applying the correction
 for(i in 1:15){
   list_correction <- diffByQuartile("high07",list_group[[i]])
   list_correction[1, correction := list_correction[2, correction]*2 - list_correction[3, correction]]
+  list_correction[, group := list_group[[i]]]
+  dt_correction <- rbind(dt_correction, list_correction)
   
   rtPeptide[rtsec <= list_correction[2, quartiles], group_quartile := as.character(1)]
   rtPeptide[rtsec > list_correction[2, quartiles] &
@@ -42,9 +53,20 @@ for(i in 1:15){
 rtPeptide["high07", corrected_RT := rtsec]
 
 setkey(rtPeptide, modified_sequence)
-rtPeptide <- rtPeptide[corrected_RT > 700 & corrected_RT < 1770]
+# ggplot(rtPeptide, aes(rtsec)) + xlim(600,2400)+ geom_histogram(aes(y = ..density..), binwidth = 20)
+# ggplot(rtPeptide, aes(corrected_RT)) + xlim(600,2400)+ geom_histogram(aes(y = ..density..), binwidth = 20)
+
+rtPeptide <- rtPeptide[corrected_RT > 700 & corrected_RT < 1860]
 # Filter for non linearly correted rt
 # rtPeptide <- rtPeptide[corrected_RT > 700 & corrected_RT < 1810]
+rtPeptide[, raw_corrected_RT := corrected_RT]
+# rtPeptide[, corrected_RT := raw_corrected_RT]
+rtPeptide[raw_corrected_RT <= 1170, corrected_RT := 320 + 0.77778 * raw_corrected_RT]
+rtPeptide[raw_corrected_RT > 1170, corrected_RT := 320 + 0.77778 * raw_corrected_RT]
+
+# To exclude redundant peptides
+rtPeptide <- rtPeptide[index_rt2 <2]
+
 rtPeptide[, q50_5 := quantile(corrected_RT, probs = 0.50), by = c("modified_sequence")]
 
 setkey(rtPeptide, index, modified_sequence)
@@ -74,7 +96,6 @@ write.csv(rtPeptide, file = paste0(projectPath,"/data/filtered_id.csv"))
 
 
 ####
-rtPeptide <- rtPeptide[corrected_RT > 700]
 
 rtPeptide[,rank_peptide := as.character(rank_peptide)]
 setkey(rtPeptide, rank_peptide)
@@ -85,10 +106,11 @@ rtPeptide[, sdDrop := ((sdReduction / sdRawRT))*100]
 
 unique(rtPeptide)[as.character(3001:3100)][, list(modified_sequence, sdRawRT, sdNewRT, sdReduction, sdDrop, nbProjPep)]
 
-ggplot(rtPeptide["50"], aes(rtsec)) + xlim(800,2000) + ylim(0,0.02) + geom_histogram(aes(y = ..density..), binwidth = 10)
-ggplot(rtPeptide["50"], aes(corrected_RT)) + xlim(800,2000) + ylim(0,0.02) + geom_histogram(aes(y = ..density..), binwidth = 10)
+ggplot(rtPeptide["2"], aes(rtsec)) + xlim(800,2000) + ylim(0,0.02) + geom_histogram(aes(y = ..density..), binwidth = 10)
+ggplot(rtPeptide["2"], aes(corrected_RT)) + xlim(800,2000) + ylim(0,0.02) + geom_histogram(aes(y = ..density..), binwidth = 10)
 
 ggplot(rtPeptide[nbProjPep <= 8], aes(sdReduction)) + geom_histogram(aes(y = ..density..), binwidth = 3)
+ggplot(rtPeptide[nbProjPep > 2], aes(sdDrop)) + xlim(-30, 100)+ geom_histogram(aes(y = ..density..), binwidth = 5)
 
 ggplot(rtPeptide[nbProjPep > 1], aes(sdRawRT)) + xlim(0,120) + ylim(0,0.05) + geom_histogram(aes(y = ..density..), binwidth = 3)
 ggplot(rtPeptide[nbProjPep > 1], aes(sdNewRT)) + xlim(0,120) + ylim(0,0.05) + geom_histogram(aes(y = ..density..), binwidth = 3)
